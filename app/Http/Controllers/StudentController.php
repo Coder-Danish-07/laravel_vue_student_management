52763eb004eab3f2dafec45642f6b86bbbff6a04
DanishSoftgetix <danish@softgetix.in>
2024-11-29 11:49:27 +0530

    search add

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,14 +12,31 @@ use App\Http\Requests\UpdateStudentRequest;
 
 class StudentController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
 
-        $students = StudentResource::collection(Student::paginate(10));
+        $studentQuery = Student::query();
+
+        $this->applySearch($studentQuery, $request->search);
+
+        $students = StudentResource::collection(
+            // Student::paginate(10)
+            $studentQuery->paginate(10)
+        );
 
         return inertia('Students/Index',[
-            'students' => $students
+            'students' => $students,
+            'search' => $request->search ?? ''
         ]);
+
     }
+
+    protected function applySearch($query, $search)
+        {
+            return $query->when($search, function ($query, $search) {
+                $query->where('name', 'like', '%' . $search . '%')
+                    ->orWhere('email', 'like', '%' . $search . '%');
+            });
+        }
 
 
     public function create(){
